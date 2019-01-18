e601fb56607deb64f87bb1846193e30b89d6d30f
sudo-potato <37392875+sudo-potato@users.noreply.github.com>
2019-01-17 19:35:25 -0500

    Create buffer.c

---
+++ os-finalproject/buffer.c
@@ -0,0 +1,143 @@
+/* *************************************************************/
+/* Nicholas Mercurio, Jay Lansiquot, Gabe Miller, Matthew Wong */
+/* SYST 44288                                                  */
+/* buffer.c                                                    */
+/* 11/18/2018                                                  */
+/* *************************************************************/
+
+
+#include "buffer.h"
+#include <stdio.h>
+#include <stdlib.h> //For rand()
+#include <pthread.h>
+#include <semaphore.h>
+#include <unistd.h> // For sleep()
+
+#define TRUE 1
+
+// Buffer intitialization
+buffer_item buffer[BUFFER_SIZE];
+
+pthread_mutex_t mutex;
+sem_t empty;
+sem_t full;
+
+int insertPointer = 0, removePointer = 0;
+
+// Product & Consumer Prototypes
+void *producer(void *param);
+void *consumer(void *param);
+
+int insert_item(buffer_item item) {
+        int return_val = 0;   
+	sem_wait(&empty);
+
+	pthread_mutex_lock(&mutex);
+	if (insertPointer < BUFFER_SIZE) {
+		buffer[insertPointer++] = item;
+		insertPointer = insertPointer % 5;
+	}
+	else {
+	    return_val = -1;
+	}
+	pthread_mutex_unlock(&mutex);
+        sem_post(&full);
+
+	return return_val;	
+}
+
+int remove_item(buffer_item *item) {
+	int return_val;
+	sem_wait(&full);
+
+	pthread_mutex_lock(&mutex);
+	if (insertPointer > 0) {
+		*item = buffer[removePointer];
+		buffer[removePointer++] = -1;
+		removePointer = removePointer % 5;
+   	}
+	else {
+        	return_val = -1;
+   	}
+	pthread_mutex_unlock(&mutex);
+	sem_post(&empty);
+
+	return return_val;	
+}
+
+
+int main(int argc, char *argv[]) {
+	int sleepTime, producerThreads, consumerThreads;
+	int i, j;
+
+	if(argc != 4) {
+		fprintf(stderr, "Usage: <sleep time> <producer threads> <consumer threads>\n");
+		return -1;
+	}
+
+	sleepTime = atoi(argv[1]);
+	producerThreads = atoi(argv[2]);
+	consumerThreads = atoi(argv[3]);
+
+	// Initialize the synchronization tools
+	printf("%d\n",pthread_mutex_init(&mutex, NULL));
+	printf("%d\n",sem_init(&empty, 0, 5));
+	printf("%d\n",sem_init(&full, 0, 0));
+	srand(time(0));
+
+	// Create the producer and consumer threads
+	for(i = 0; i < producerThreads; i++) {
+		pthread_t tid;
+		pthread_attr_t attr;
+		pthread_attr_init(&attr);
+		pthread_create(&tid, &attr, producer, NULL);
+	}
+
+	for(j = 0; j < consumerThreads; j++) {
+		pthread_t tid;
+		pthread_attr_t attr;
+		pthread_attr_init(&attr);
+		pthread_create(&tid, &attr, consumer, NULL);
+	}
+
+	/* Sleep for user specified time */
+	sleep(sleepTime);
+	return 0;
+}
+
+void *producer(void *param) {
+	buffer_item random;
+	int r;
+
+	while(TRUE) {
+		r = rand() % 5;
+		sleep(r);
+		random = rand();
+
+		// Checks if there is an error inserting random number
+		if(insert_item(random)) {
+			fprintf(stderr, "Error");
+		}
+
+		printf("Producer produced %d \n", random);
+
+	}
+
+}
+
+void *consumer(void *param) {
+	buffer_item random;
+	int r;
+
+	while(TRUE) {
+		r = rand() % 5;
+		sleep(r);
+
+		if(remove_item(&random)) {
+			fprintf(stderr, "Error Consuming \n");
+		}
+		else {
+			printf("Consumer consumed %d \n", random);
+		}
+	}
+}
