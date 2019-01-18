c7016e4835ce5b04a167146765ac9dc8688489b5
sudo-potato <37392875+sudo-potato@users.noreply.github.com>
2019-01-17 19:35:52 -0500

    Create buffer.h

---
+++ os-finalproject/buffer.h
@@ -0,0 +1,9 @@
+/* *************************************************************/
+/* Nicholas Mercurio, Jay Lansiquot, Gabe Miller, Matthew Wong */
+/* SYST 44288                                                  */
+/* buffer.h                                                    */
+/* 11/18/2018                                                  */
+/* *************************************************************/
+
+typedef int buffer_item;
+#define BUFFER_SIZE 5
